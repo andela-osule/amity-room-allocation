d00cc31a29ff52363cc3f7d68f9e99099591d3c5
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-13 19:01:55 +0100

    (feature) add file parser

--- amity/tools.py
+++ amity/tools.py
@@ -1,3 +1,7 @@
+from re import search
+from people import Person
+
+
 def flatten(args):
     """This function flattens a nested list.
     """
@@ -7,7 +11,36 @@ def flatten(args):
         return [args]
 
 
-def  get_parent(arg):
+def get_parent(arg):
     """Return parent class of the arg
     """
     return arg.__class__.__base__
+
+
+class PeopleFileParser:
+
+    @staticmethod
+    def parse_persons(datafile='amity/data_samples/people.txt',
+                      add=True,
+                      output=None):
+        """Parses person from a file.
+        "  Call with datafile path.
+        "  Optional arguments are <add> and <output>
+        """
+        persons = []
+        with open(datafile, 'r') as people_file:
+            for line in people_file:
+                s = search('^(\w+\s[^\s]+)[\s]{1,}(\w+)[\s]{0,}(\w)?', line)
+                g = s.groups()
+                p = Person(g[0]).make_person(g[1])
+                persons.append(p)
+        if add:
+            from building import Amity
+            Amity.add_persons(persons)
+        if output is 'print':
+            print persons
+        return persons
+
+    @staticmethod
+    def line_to_person(datafile):
+        return PeopleFileParser.parse_persons(datafile)
