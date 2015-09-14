fc3e44d6cabb8d1230dc787bbc45e32500d7ebac
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-14 04:59:15 +0100

    (feature) add ability for Manager to assign person to office, person to living spaces and to allocate randomly

--- amity/alloc.py
+++ amity/alloc.py
@@ -1,5 +1,6 @@
-from people import Manager, Person
-from building import Room, Amity
+from people import Manager, Staff, People
+from building import Room, Amity, LivingSpace, Office
+from random import randint
 
 
 def generate_rooms(room_type, n=10):
@@ -7,43 +8,92 @@ def generate_rooms(room_type, n=10):
        It should be called with <room_type> and <n:number_of_rooms>
        It returns list of room objects.
     """
-    start = Amity.room_count + 1
-    rooms = [Room("Room {0}".format(i), room_type) for i in xrange(start, n+1)]
+    start = Amity.room_count or Amity.room_count + 1
+    rooms = [Room("Room {0}".format(i),
+             room_type) for i in xrange(start, start+n)]
     return rooms
 
 
-def assign_to_office(self):
-    print self.name
+@staticmethod
+def assign_to_office(office, person):
+    """Assign person to an office.
+    """
+    if not office.filled():
+        office.add_occupant(person)
 
 
+
+@staticmethod
 def get_list_of_allocations():
     """This gets the list of allocations
     """
     pass
 
 
+@staticmethod
 def print_list_of_allocations():
     """This prints the list of allocations
     """
     pass
 
 
+@staticmethod
 def get_list_of_unallocated():
     """This gets the list of unallocated people
     """
     pass
 
 
+@staticmethod
 def print_members_in_room(room_name):
     """Given a room, this prints all members in it.
     """
     pass
 
 
-def assign_to_room(self):
-    pass
+@staticmethod
+def assign_to_room(room_name, person_name):
+    """Assign a person to a room.
+    """
+    person = Amity.find_person(person_name)
+    room = Amity.find_room(room_name)
+    if isinstance(room, Office) and not person.has_office():
+        Manager.assign_to_office(room, person)
+    elif isinstance(room, LivingSpace) and not isinstance(person, Staff):
+        Manager.assign_to_living_space(room, person)
 
+
+@staticmethod
+def assign_to_living_space(living_space, person):
+    """Assign a person to a living space.
+    """
+    if person.has_expressed_interest():
+        if person.is_female():
+            if living_space.has_no_occupant()\
+               or (living_space.has_female_occupant()
+               and not living_space.filled()):
+                living_space.add_occupant(person)
+        else:
+            if living_space.has_no_occupant() or not living_space.filled():
+                living_space.add_occupant(person)
+
+
+@staticmethod
+def allocate():
+    while not (Amity.all_rooms_filled() or People.all_assigned()):
+        Manager.assign_to_living_space(
+                                    Amity.room_collection[randint(0, 20)],
+                                    Amity.people_collection[randint[0, 20]]
+                                    )
+
+
+Manager.assign_to_room = assign_to_room
 Manager.assign_to_office = assign_to_office
+Manager.assign_to_living_space = assign_to_living_space
+Manager.allocate = allocate
+Manager.get_list_of_unallocated = get_list_of_unallocated
+Manager.print_list_of_allocations = print_list_of_allocations
+Manager.print_members_in_room = print_members_in_room
 
 if __name__ == '__main__':
     offices = generate_rooms('office')
