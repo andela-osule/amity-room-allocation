3373650da6f71bf938d07b7272fbcef2858a166e
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-14 04:50:20 +0100

    (feature) test that rooms can be added and removed; also test that persons can be assigned to rooms

--- amity/test_amity.py
+++ amity/test_amity.py
@@ -8,28 +8,31 @@ import unittest
 import sys
 import os.path
 
+file_path = 'amity/data_samples/people.txt'
 
-class AmityTestCase(unittest.TestCase):
+
+class aAmityTestCase(unittest.TestCase):
 
     def setUp(self):
         self.r = Room('Anvil', 'office')
         self.l = Room('Ruby', 'living space')
         self.f = Person('Jack').make_person('fellow')
         self.s = Person('Mark').make_person('staff')
-        self.amity = Amity
 
     def test_can_create_office_or_living_space(self):
         self.assertIsInstance(self.r, Office)
         self.assertIsInstance(self.l, LivingSpace)
 
     def test_can_add_room_to_amity(self):
-        self.amity.add_room(self.r)
-        self.amity.add_room(self.l)
-        self.assertEquals(self.amity.room_count, 2)
+        Amity.reset_room_count()
+        Amity.add_room(self.r)
+        Amity.add_room(self.l)
+        self.assertEquals(Amity.room_count, 2)
 
     def test_can_remove_room(self):
-        self.amity.remove_room(self.r.name)
-        self.assertEquals(self.amity.room_count, 1)
+        Amity.room_collection
+        Amity.remove_room(self.r.name)
+        self.assertEquals(Amity.room_count, 1)
 
     def test_can_create_fellow_or_staff(self):
         self.assertIsInstance(self.f, Fellow)
@@ -38,31 +41,34 @@ class AmityTestCase(unittest.TestCase):
 
 class AllocTestCase(unittest.TestCase):
 
-    def test_can_generate_offices(self):
+    def test_can_generate_and_assign_to_rooms(self):
         rooms = alloc.generate_rooms('office')
-        self.assertEquals(10, len(rooms))
+        self.assertEquals(len(rooms), 10)
         Amity.add_rooms(rooms)
         self.assertIsInstance(rooms[randint(0, 9)], Office)
-
-    def test_can_generate_living_spaces(self):
         rooms = alloc.generate_rooms('living space')
-        self.assertEquals(10, len(rooms))
+        self.assertEquals(len(rooms), 10)
         Amity.add_rooms(rooms)
         self.assertIsInstance(rooms[randint(0, 9)], LivingSpace)
         self.assertEqual(Amity.room_count, 20)
 
+        PeopleFileParser.line_to_person(file_path)
+        print Amity.room_count
+        person_name = 'ANDREW PHILLIPS'
+        Manager.assign_to_room('Room 1', person_name)
+        Manager.assign_to_room('Room 10', person_name)
+        person = Amity.find_person(person_name)
+        self.assertEquals(person.office.name, 'Room 1')
+        self.assertIn(person, Amity.find_room('Room 1').occupants)
+
     def test_can_make_allocations(self):
         Manager.allocate()
-        self.assertIsNone(alloc.get_list_of_unallocated())
-
-    def test_can_assign_to_office(self):
-        Manager.assign_to_office()
+        self.assertIsNone(Manager.get_list_of_unallocated())
 
 
 class PeopleFileParserTestCase(unittest.TestCase):
 
     def test_can_parse_people_from_file(self):
-        file_path = 'amity/data_samples/people.txt'
         persons = PeopleFileParser.line_to_person(file_path)
         self.assertEquals(len(persons), 5)
         self.assertIsInstance(persons[randint(0, 4)], Person)
