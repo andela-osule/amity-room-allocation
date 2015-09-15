a7930f742bc871ee045762ff0ceddd2220c46dd0
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-15 16:21:30 +0100

    (fix) move random getters to building.py; create add_occupant method of LivingSpace & find_living_space_with_female_occupant

--- amity/building.py
+++ amity/building.py
@@ -1,4 +1,6 @@
 import tools
+from random import randint
+from exception import OutOfOfficeException, OutOfLivingSpaceException
 
 
 class Room(object):
@@ -11,19 +13,24 @@ class Room(object):
         self.__init__(name)
 
     def set_name(self, name):
+        """Assign name to a room.
+        """
         self.name = name
 
     def get_capacity(self):
+        """Get the capacity of a room.
+        """
         return self.get_capacity()
 
     def filled(self):
-        return True if len(self.occupants) is self.capacity else False
-
-    def add_occupant(self, person):
-        """Adds an occupant to a room.
+        """Check if a room is filled with people
         """
-        self.occupants.append(person)
-        person.assign_office(self)
+        return True if len(self.occupants) == self.capacity else False
+
+    def has_no_occupant(self):
+        """Check if a room has an occupant.
+        """
+        return True if len(self.occupants) == 0 else False
 
 
 class Office(Room):
@@ -35,6 +42,12 @@ class Office(Room):
 
     def __init__(self, name):
         self.set_name(name)
+
+    def add_occupant(self, person):
+        """Add an occupant to a room.
+        """
+        self.occupants.append(person)
+        person.assign_office(self)
 
     def __repr__(self):
         return "{0} (OFFICE)".format(self.name)
@@ -51,12 +64,18 @@ class LivingSpace(Room):
         self.set_name(name)
 
     def has_female_occupant(self):
-        female_occupant = (True for person
-                           in self.occupants
-                           if person.is_female())
-        if len(female_occupant) > 0:
-            return True
+        """Check if a female occupant is present in the room
+        """
+        for person in self.occupants:
+            if person.is_female():
+                return True
         return False
+
+    def add_occupant(self, person):
+        """Add an occupant to a room.
+        """
+        self.occupants.append(person)
+        person.assign_living_space(self)
 
     def __repr__(self):
         return "{0} (LIVING)".format(self.name)
@@ -90,11 +109,13 @@ class Amity:
 
     @staticmethod
     def update_room_count():
+        """Update the room count as room collection changes.
+        """
         Amity.room_count = len(Amity.room_collection)
 
     @staticmethod
     def remove_room(room_name):
-        """Removes a room by name from the building.
+        """Remove a room by name from the building.
         """
         room_space = Amity.find_room(room_name)
         try:
@@ -105,7 +126,7 @@ class Amity:
 
     @staticmethod
     def remove_rooms(*room_names):
-        """Removes multiple rooms from the buiding.
+        """Remove multiple rooms from the buiding.
            Arguments can be a list of rooms or comma-separated values
         """
         room_name_list = tools.flatten(list(room_names))
@@ -114,13 +135,16 @@ class Amity:
 
     @staticmethod
     def find_room(room_name):
+        """Find a room by name
+        """
         for room in Amity.room_collection:
             if room.name == room_name:
                 return room
+        return None
 
     @staticmethod
     def add_person(person):
-        """Adds a person to the people collection
+        """Add a person to the people collection
         """
         Amity.people_collection.append(person)
 
@@ -134,33 +158,68 @@ class Amity:
 
     @staticmethod
     def reset_room_count():
-        """Resets room count to zero.
+        """Reset room count to zero
         """
         Amity.remove_rooms([room.name for room in Amity.room_collection])
 
     @staticmethod
     def find_person(person_name):
-        """Finds a person in the people_collection by <person_name>
+        """Find a person in the people_collection by <person_name>
            and returns a person object instance from the collection
         """
-        person_tuple = (person for person in Amity.people_collection
-                        if person.name == person_name)
-        return person_tuple.next()
+        for person in Amity.people_collection:
+            if person.name == person_name:
+                return person
+        return None
 
     @staticmethod
     def all_rooms_filled():
-        available_rooms = [True
-                           for room
-                           in Amity.room_collection
-                           if not room.filled()]
-        if len(available_rooms) > 0:
-            return False
+        """Check if all rooms are filled
+        """
+        for room in Amity.room_collection:
+            if not room.filled():
+                return False
         return True
 
     @staticmethod
     def all_persons_assigned():
-        unassigned_persons = [True
-                              for person in Amity.people_collection
-                              if not person.has_office()]
-        if len(unassigned_persons) > 0:
-            return True
+        """Check is all every person has room
+        """
+        for person in Amity.people_collection:
+            if not person.has_office():
+                return False
+        return True
+
+    @staticmethod
+    def find_living_space_with_female_occupant():
+        """Find a living space with a female occupant
+        """
+        for room in Amity.room_collection:
+            if room.has_female_occupant() and not room.filled():
+                return room
+        return None
+
+    @staticmethod
+    def get_random_office():
+        """Get a random office
+        """
+        offices = [room for room in Amity.room_collection
+                   if isinstance(room, Office) and not room.filled()]
+        try:
+            o = offices[randint(0, len(offices)-1)]
+            return o
+        except:
+            raise OutOfOfficeException()
+
+    @staticmethod
+    def get_random_living_space():
+        """Get a random living space
+        """
+        living_spaces = [room for room in Amity.room_collection
+                         if isinstance(room, LivingSpace)
+                         and not room.filled()]
+        try:
+            l = living_spaces[randint(0, len(living_spaces)-1)]
+            return l
+        except:
+            raise OutOfLivingSpaceException()
